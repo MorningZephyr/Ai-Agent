e87afff72846a791782524f7e7f31dd17662d3b2
Zephyr <zhentaop118@gmail.com>
2025-07-21 21:57:26 -0400

    added code to create db file for saving information

---
+++ short-form-team/main.py
@@ -0,0 +1,22 @@
+from google.adk.agents import Agent
+from google.adk.runners import Runner
+from google.adk.sessions import DatabaseSessionService
+import asyncio
+import dotenv
+
+dotenv.load_dotenv()
+
+ # --- Create Database if not already exist
+db_url = "sqlite:///script.db"
+session_service =  DatabaseSessionService(db_url=db_url)
+
+
+
+async def main():
+    pass
+   
+
+
+
+if __name__ == "__main__":    
+    asyncio.run(main())
---
+++ short-form-team/script_agent/agent.py
@@ -0,0 +1,8 @@
+from google.adk.agents import Agent
+
+
+script_agent = Agent(
+    name="script_agent",
+    description="An agent that can write scripts based on user what user describes.",
+    instruction=""
+)
